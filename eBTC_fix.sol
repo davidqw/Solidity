3ae9ec53ea7c1d73659510102db9c20abe9085ae
Orange <orangemtony@gmail.com>
2017-10-09 11:47:06 -0500

    add origin

--- eBTC_fix.sol
+++ eBTC_fix.sol
@@ -49,6 +49,16 @@ pragma solidity ^0.4.16;
         }
      }
      
+     /*
+          function distributeEBTC(address[] addresses) onlyOwner {
+         for (uint i = 0; i < addresses.length; i++) {
+             balances[owner] -= 245719916000;
+             balances[addresses[i]] += 245719916000;
+             Transfer(owner, addresses[i], 245719916000);
+         }
+     }
+     */
+     
   
      function totalSupply() constant returns (uint256 totalSupply) {
          totalSupply = _totalSupply;
