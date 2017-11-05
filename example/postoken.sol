812402980e5101532b4d7475b4e66e2574fcc8ff
OrangeM21 <orangemtony@gmail.com>
2017-11-06 01:11:15 +0800

    review_POStoken

--- example/postoken.sol
+++ example/postoken.sol
@@ -6,6 +6,7 @@ pragma solidity ^0.4.11;
  * @dev Math operations with safety checks that throw on error
  */
 library SafeMath {
+
     function mul(uint256 a, uint256 b) internal constant returns (uint256) {
         uint256 c = a * b;
         assert(a == 0 || c / a == b);
@@ -44,6 +45,8 @@ contract Ownable {
     /**
      * @dev The Ownable constructor sets the original `owner` of the contract to the sender
      * account.
+     开发者用这个函数来定义owner 下面一定会用到
+     执行
      */
     function Ownable() {
         owner = msg.sender;
@@ -52,6 +55,8 @@ contract Ownable {
 
     /**
      * @dev Throws if called by any account other than the owner.
+     修改前的校验
+     OnlyOwner
      */
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -62,6 +67,7 @@ contract Ownable {
     /**
      * @dev Allows the current owner to transfer control of the contract to a newOwner.
      * @param newOwner The address to transfer ownership to.
+     修改所属权 只有owner能做修改
      */
     function transferOwnership(address newOwner) onlyOwner {
         require(newOwner != address(0));
@@ -73,6 +79,7 @@ contract Ownable {
 
 /**
  * @title ERC20Basic
+ ERC20的底层
  * @dev Simpler version of ERC20 interface
  * @dev see https://github.com/ethereum/EIPs/issues/179
  */
@@ -86,6 +93,7 @@ contract ERC20Basic {
 
 /**
  * @title ERC20 interface
+ ERC20额外的接口
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20 is ERC20Basic {
@@ -99,19 +107,26 @@ contract ERC20 is ERC20Basic {
 /**
  * @title PoSTokenStandard
  * @dev the interface of PoSTokenStandard
+POStoken增加的接口
  */
 contract PoSTokenStandard {
     uint256 public stakeStartTime;
     uint256 public stakeMinAge;
     uint256 public stakeMaxAge;
+    // mint()方法 返回bool
     function mint() returns (bool);
+    //币龄接口 返回数字
     function coinAge() constant returns (uint256);
+    // ??
     function annualInterest() constant returns (uint256);
+    //日志
     event Mint(address indexed _address, uint _reward);
 }
 
 
 contract PoSToken is ERC20,PoSTokenStandard,Ownable {
+
+    //使用library for uint256
     using SafeMath for uint256;
 
     string public name = "PoSToken";
@@ -124,51 +139,60 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
     uint public stakeMinAge = 3 days; // minimum age for coin age: 3D
     uint public stakeMaxAge = 90 days; // stake age of full weight: 90D
     uint public maxMintProofOfStake = 10**17; // default 10% annual interest
+    //货币总量的10% 来做miner
 
-    uint public totalSupply;
+    uint public totalSupply; //
     uint public maxTotalSupply;
     uint public totalInitialSupply;
+//结构 transferInStruct
+//数量 时间
 
     struct transferInStruct{
     uint128 amount;
     uint64 time;
     }
 
-    mapping(address => uint256) balances;
+    
+    //前两个map是标准结构
+    mapping(address => uint256) balances; 
     mapping(address => mapping (address => uint256)) allowed;
-    mapping(address => transferInStruct[]) transferIns;
 
+    //后一个map是自定义的
+    mapping(address => transferInStruct[]) transferIns;
+    //日志
     event Burn(address indexed burner, uint256 value);
 
     /**
      * @dev Fix for the ERC20 short address attack.
+     修正ERC20 下溢漏洞
+     https://ericrafaloff.com/analyzing-the-erc20-short-address-attack/
      */
     modifier onlyPayloadSize(uint size) {
         require(msg.data.length >= size + 4);
         _;
     }
-
+    //验证是否可以挖矿
     modifier canPoSMint() {
         require(totalSupply < maxTotalSupply);
         _;
     }
-
+    //主程序
     function PoSToken() {
         maxTotalSupply = 10**25; // 10 Mil.
         totalInitialSupply = 10**24; // 1 Mil.
 
-        chainStartTime = now;
-        chainStartBlockNumber = block.number;
+        chainStartTime = now;//部署时间
+        chainStartBlockNumber = block.number; //部署区块
 
         balances[msg.sender] = totalInitialSupply;
-        totalSupply = totalInitialSupply;
+        totalSupply = totalInitialSupply;  //10**24
     }
 
     function transfer(address _to, uint256 _value) onlyPayloadSize(2 * 32) returns (bool) {
-        if(msg.sender == _to) return mint();
+        if(msg.sender == _to) return mint(); //自己发代币给自己 返回mint()方法
         balances[msg.sender] = balances[msg.sender].sub(_value);
         balances[_to] = balances[_to].add(_value);
-        Transfer(msg.sender, _to, _value);
+        Transfer(msg.sender, _to, _value);//日志记录
         if(transferIns[msg.sender].length > 0) delete transferIns[msg.sender];
         uint64 _now = uint64(now);
         transferIns[msg.sender].push(transferInStruct(uint128(balances[msg.sender]),_now));
@@ -192,13 +216,14 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
         balances[_to] = balances[_to].add(_value);
         allowed[_from][msg.sender] = _allowance.sub(_value);
         Transfer(_from, _to, _value);
-        if(transferIns[_from].length > 0) delete transferIns[_from];
+        if(transferIns[_from].length > 0) delete transferIns[_from];//删除transferIns历史数据
         uint64 _now = uint64(now);
-        transferIns[_from].push(transferInStruct(uint128(balances[_from]),_now));
-        transferIns[_to].push(transferInStruct(uint128(_value),_now));
+        transferIns[_from].push(transferInStruct(uint128(balances[_from]),_now));//重新计算币龄
+        transferIns[_to].push(transferInStruct(uint128(_value),_now));//重新计算币龄
         return true;
     }
-
+    //标准的erc20做法
+    //注意，权益没有转移
     function approve(address _spender, uint256 _value) returns (bool) {
         require((_value == 0) || (allowed[msg.sender][_spender] == 0));
 
@@ -210,31 +235,40 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
         return allowed[_owner][_spender];
     }
+    //挖矿方法
+    /* 做验证
+        modifier canPoSMint() {
+        require(totalSupply < maxTotalSupply);
+        _;
+    }
+    */
+
 
     function mint() canPoSMint returns (bool) {
         if(balances[msg.sender] <= 0) return false;
-        if(transferIns[msg.sender].length <= 0) return false;
+        if(transferIns[msg.sender].length <= 0) return false; //没有币龄  失败
 
         uint reward = getProofOfStakeReward(msg.sender);
         if(reward <= 0) return false;
 
-        totalSupply = totalSupply.add(reward);
-        balances[msg.sender] = balances[msg.sender].add(reward);
-        delete transferIns[msg.sender];
+        totalSupply = totalSupply.add(reward); //增加存世总量
+        balances[msg.sender] = balances[msg.sender].add(reward);//加奖金
+        delete transferIns[msg.sender];//删除旧币龄
         transferIns[msg.sender].push(transferInStruct(uint128(balances[msg.sender]),uint64(now)));
+        //增加新币龄
 
-        Mint(msg.sender, reward);
+        Mint(msg.sender, reward);// MInt() 日志记录
         return true;
     }
-
+    // 从开始到现在 多少区块
     function getBlockNumber() returns (uint blockNumber) {
         blockNumber = block.number.sub(chainStartBlockNumber);
     }
-
+    //币龄计算
     function coinAge() constant returns (uint myCoinAge) {
         myCoinAge = getCoinAge(msg.sender,now);
     }
-
+    //收益率
     function annualInterest() constant returns(uint interest) {
         uint _now = now;
         interest = maxMintProofOfStake;
@@ -246,10 +280,12 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
     }
 
     function getProofOfStakeReward(address _address) internal returns (uint) {
+        //判断时间
         require( (now >= stakeStartTime) && (stakeStartTime > 0) );
-
+        //定义函数内时间
         uint _now = now;
         uint _coinAge = getCoinAge(_address, _now);
+        //计算币龄
         if(_coinAge <= 0) return 0;
 
         uint interest = maxMintProofOfStake;
@@ -265,7 +301,7 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
 
         return (_coinAge * interest).div(365 * (10**decimals));
     }
-
+    //获取币龄
     function getCoinAge(address _address, uint _now) internal returns (uint _coinAge) {
         if(transferIns[_address].length <= 0) return 0;
 
@@ -278,12 +314,12 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
             _coinAge = _coinAge.add(uint(transferIns[_address][i].amount) * nCoinSeconds.div(1 days));
         }
     }
-
+    //修改时间
     function ownerSetStakeStartTime(uint timestamp) onlyOwner {
         require((stakeStartTime <= 0) && (timestamp >= chainStartTime));
         stakeStartTime = timestamp;
     }
-
+    //烧币
     function ownerBurnToken(uint _value) onlyOwner {
         require(_value > 0);
 
@@ -297,7 +333,7 @@ contract PoSToken is ERC20,PoSTokenStandard,Ownable {
 
         Burn(msg.sender, _value);
     }
-
+    //批量转币
     /* Batch token transfer. Used by contract creator to distribute initial tokens to holders */
     function batchTransfer(address[] _recipients, uint[] _values) onlyOwner returns (bool) {
         require( _recipients.length > 0 && _recipients.length == _values.length);
